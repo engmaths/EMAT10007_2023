c530d4603e1b3ff7f3c216437b874973d0293ecc
Matthew Hennessy <hennessy.matt@gmail.com>
2023-11-19 18:11:02 +0000

    update 10 material

--- weekly_content/example_code/w10_buggy_integrator.py
+++ weekly_content/example_code/w10_buggy_integrator.py
@@ -1,18 +1,18 @@
 from math import *
-import matplotlib.pyplot as plt
 
 # Define the parameters in the problem
+alpha = 10
+mu = 1.5
+
 a = 1
 b = 3
 
 N = 10
 dx = (b - a) / N
 
-alpha = 10
-mu = 1.5
 
 
-# Define the mathematical function to integrate over
+# Define the mathematical function to compute the area of
 def f(x):
 
     # make mu and alpha global variables so they can
@@ -24,18 +24,20 @@ def f(x):
     # return the value of the function
     return alpha * exp(-mu * x)
 
-# Define the exact value of the integral for comparison
+
+# Define and print exact value of the area for comparison
 A_exact = alpha / mu * (exp(-mu * a) - exp(-mu * b))
+print('The exact area is', A_exact)
 
 
-# Define a Python function to approximate the integral using
+# Define a Python function to approximate the area using
 # the trapezium rule
 def integrate(N):
 
     # initialise the area under the curve
     A = 1/2 * (f(a) + f(b)) * dx
 
-    # compute the sum in the integration formula
+    # compute the sum in the formula
     for i in range(1, N):
         x_i = a + i * dx
         A += f(x_i) * dx
@@ -44,7 +46,7 @@ def integrate(N):
     return A
 
 
-# Now we use a loop to compute the integrals for different
+# Use a loop to compute the integrals for different
 # values of N (number of trapezoids)
 
 N_list = [2**n for n in range(2, 7)]
@@ -54,4 +56,3 @@ for n, N in enumerate(N_list):
     A_list.append(integrate(N))
     print('The area when N =', N, 'is:', A_list[n])
 
-print('The exact area is', A_exact)
